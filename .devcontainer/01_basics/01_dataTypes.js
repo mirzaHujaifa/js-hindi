77368852ff58694c01738a631eda838ee7777e82
mirzaHujaifa <164058241+mirzaHujaifa@users.noreply.github.com>
2025-03-12 07:44:54 +0000

    datatype

---
+++ .devcontainer/01_basics/01_dataTypes.js
@@ -0,0 +1,13 @@
+let age=15
+let isLoggedIn= false
+let state;
+
+
+//deta types
+//number => 2 to power 53
+//bigint for larger numbe 
+//boolean used for true/fale
+//string for name
+//null=> standalon value / object
+// undefined =>
+    console.log(typeof null)
---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accoountId="78966"
+let accountEmail="mirza@gmail"
+accoountCity="kolkata"
+var accountPassword='1345'
+let accountState="west Bengal"//you can uesd or nor semicolon
+
+accountEmail="herok@gmail.com"
+accountPassword="1345"
+
+
+console.log(accoountId)
+console.table([accoountId,accountEmail,accountPassword,accountState,accoountCity])
